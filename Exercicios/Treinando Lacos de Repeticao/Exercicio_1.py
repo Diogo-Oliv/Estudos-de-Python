8495026d882a9660654735f14d198d32040a71e6
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-11 13:21:06 -0300

    Create Exercicio_1.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_1.py
@@ -0,0 +1,14 @@
+# Exercício 1 - Entendendo Laços.
+
+# Desenvolva um programa que processe uma lista de n nomes de clientes.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Lista de clientes.
+
+clientes = ["Andre", "João", "Carlos", "Leonardo", "Luiz"]
+
+# Laço de repetição para iterar os nomes dentro da lista de clientes.
+
+for i in clientes:
+  print(i)
