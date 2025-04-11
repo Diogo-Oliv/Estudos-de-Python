14fa0cc65e2477a3bc46cc1910047b72c2e14812
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-11 13:31:14 -0300

    Create Exercicio_2.py
    Criação do Setup do exercício 2

---
+++ Exercicios/Treinando Condicionais/Exercicio_2.py
@@ -0,0 +1,7 @@
+# Exercício 2 - Calculo de tempo de projeto.
+
+# É necessário criar um programa que calcule o tempo necessário para concluir um número n de atividades.
+# Escreva um programa que receba o número de atividades e calcule o tempo total.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
