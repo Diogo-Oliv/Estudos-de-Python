069661afefbf98b4965f5b38bdd607c02b7fd178
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-11 13:33:24 -0300

    Update Exercicio_1.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_1.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_1.py
@@ -7,11 +7,14 @@
 # Lista de clientes.
 clientes = []
 
+# Número de clientes escolhido pelo usuário.
 numClientes = int(input("Insira o número total de clientes: "))
 
+# Laço de repetição que usa o numClientes como contador e adiciona os clientes para a lista.
 for i in range(1, numClientes + 1):
   cliente = input(f"Insira o nome do {i}° cliente: ")
   clientes.append(cliente)
 
+# Laço para mostrar todos os valores da lista de clientes.
 for j in clientes:
   print(j)
