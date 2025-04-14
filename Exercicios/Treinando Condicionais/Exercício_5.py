5d793c2f917180fe05e7c8ef96bb0f14e1baf303
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-14 13:55:32 -0300

    Create Exercício_5.py

---
+++ Exercicios/Treinando Condicionais/Exercício_5.py
@@ -0,0 +1,5 @@
+# Exercício 5 - Controle de orçamento.
+
+# Escreva um programa que receba o total de despesas e informe se ultrapassou um limite especificado pelo usuário.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
