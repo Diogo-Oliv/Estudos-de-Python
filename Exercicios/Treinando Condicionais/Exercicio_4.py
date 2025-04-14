017a38c37c910992ccc995d8f8fb6c8a4ebbdb28
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-14 13:21:36 -0300

    Criação de exercício
    Criação e resolução do exercício 4

---
+++ Exercicios/Treinando Condicionais/Exercicio_4.py
@@ -0,0 +1,29 @@
+# Exercício 4 - Teste com Temperaturas
+
+# Escreva um programa que realize o calculo do IMC de uma pessoa, leve em consideração as seguintes métricas:
+
+# IMC menor que 18.5 = abaixo do peso.
+# IMC maior que 18.5 e menor que 25 = peso normal.
+# IMC maior que 25 = acima do peso.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Peso e Altura do usuário.
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
+
+# Operação inicial do IMC.
+IMC = peso / (altura ** 2)
+
+# Valor do IMC.
+print(f"Seu IMC é {IMC:.1f}")
+
+# Condicionais do IMC para Categorização do peso.
+if IMC <= 18.5:
+    print("Usuário abaixo do peso!")
+
+elif IMC > 18.5 and IMC <= 25.0:
+    print("Usuário de peso normal!")
+
+else:
+    print("Usuário muito acima do peso!")
