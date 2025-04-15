b87c98f02cc321f20de64c0683c286102d9d1675
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 15:50:50 -0300

    Update Exercicio_5.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_5.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_5.py
@@ -4,3 +4,17 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
+caractere = input("Insira o caractere desejado para construir o triângulo: ")
+
+for linha in range(1, 6):
+    print(caractere * linha)
+
+permissao = input("Gostaria de ver o triângulo de ponta cabeça? [S/N]").uper()
+
+if permissao == "S":
+
+    for linha in range(5, 0, -1):
+        print(caractere * linha)
+
+else:
+    print("Fim do programa.")
