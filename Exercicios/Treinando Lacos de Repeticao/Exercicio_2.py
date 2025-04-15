a3ad38f7a9a6199391c421cb23b94e78b3eb4d82
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 14:35:28 -0300

    Update Exercicio_2.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_2.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_2.py
@@ -3,3 +3,12 @@
 # Escreva um programa que utiliza um laço While para contar até um certo número escolhido previamente.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Variáveis com os valores iniciais e finais
+numFinal = int(input("Insira o número máximo da contagem: "))
+numInicial = 0
+
+# Laço While para iterar o valor de numInicial até ele se tornar maior que o numFinal + 1 para termos o valor exato.
+while numInicial < numFinal + 1:
+    print(f"num: {numInicial}")
+    numInicial += 1
