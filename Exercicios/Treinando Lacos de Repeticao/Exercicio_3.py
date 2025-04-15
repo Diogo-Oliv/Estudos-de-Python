a82dd1bfb7dda003f8d9692a1a5afcf8014fb4ab
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 14:50:13 -0300

    Create Exercicio_3.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_3.py
@@ -0,0 +1,11 @@
+# Exercício 3 - Validação de entrada.
+
+# Escreva um programa que garanta um padrão de nomes de usuários e senhas válidos seguindo as seguintes regras:
+
+# O nome de usuário deve ter, no mínimo 5 caracteres.
+# A senha deve ter, no mínimo, 10 caracteres.
+
+# Além disso, é necessário que o sistema continue pedindo informações até que todos os requisitos sejam atendidos. Quando o usuário
+# inserir dados válidos, o programa deve exibir a mensagem "Cadastro realizado.".
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
