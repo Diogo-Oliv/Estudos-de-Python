b12e1e2bfb1dc591cd1561863df64b894a0fcc8e
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 14:50:25 -0300

    Update Exercicio_3.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_3.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_3.py
@@ -9,3 +9,24 @@
 # inserir dados válidos, o programa deve exibir a mensagem "Cadastro realizado.".
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Status de validação inicialmente falso para  dar início ao loop While.
+statusInicial = False
+
+# Loop while para a repetição do processo até atender os requisitos.
+while statusInicial == False:
+  usuario = input("Digite seu nome de usuário: ")
+  senha = input("Digite sua senha: ")
+
+# Condicional caso tudo esteja de acordo.
+  if (len(usuario) >= 5) and (len(senha) >= 10):
+    print("Cadastro realizado com sucesso!")
+    statusInicial = True
+
+# Condicional para o nome de usuário.
+  elif len(usuario) < 5:
+    print("O nome de usuário deve ter pelo menos 5 caracteres")
+
+# Condicional para a senha.
+  elif len(senha) < 10:
+    print("A senha deve ter pelo menos 10 caracteres")
