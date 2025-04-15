08ecf39cf291a9b64c51b36266d4d379bd3161c1
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 01:54:55 -0300

    Create Exercício_10.py

---
+++ Exercicios/Treinando Condicionais/Exercício_10.py
@@ -0,0 +1,8 @@
+# Exercício 10 - Aprovação de crédito.
+
+# Escreva um programa para solicitar crédito, sua aprovação dependerá dos seguintes aspectos:
+
+# O valor da renda mensal deve ser maior que R$ 3.500,00
+# O valor da aprovação não pode ultrapassar 30% da renda.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
