f1a3399c1328451e6f8f6a193904f8e1a95411e3
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 15:11:53 -0300

    Create Exercicio_4.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_4.py
@@ -0,0 +1,5 @@
+# Exercício 4 - Tabuada.
+
+# Escreva um programa que imprima a tabuada de um número pedido pelo usuário até um limite também imposto por ele.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
