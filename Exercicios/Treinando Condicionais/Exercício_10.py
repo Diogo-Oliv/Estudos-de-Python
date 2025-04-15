511e15297703f793fb17e63e227fdc94b1d1d8bd
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 01:59:26 -0300

    Update Exercício_10.py

--- Exercicios/Treinando Condicionais/Exercício_10.py
+++ Exercicios/Treinando Condicionais/Exercício_10.py
@@ -6,3 +6,18 @@
 # O valor da aprovação não pode ultrapassar 30% da renda.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+#Coleta de dados
+rendaMensal = float(input("Digite o valor da sua renda mensal: "))
+parcela = float(input("Digite o valor do crédito desejado: "))
+
+porcentagem = (parcela / rendaMensal) * 100
+
+if (rendaMensal > 3500) and (parcela <= 30):
+  print("Crédito aceito.")
+
+elif rendaMensal <= 3500:
+  print("Crédito negado: renda insuficiente.")
+
+else:
+  print(f"Crédito negado: Parcela acima de {porcentagem:.0f}% da renda.")
