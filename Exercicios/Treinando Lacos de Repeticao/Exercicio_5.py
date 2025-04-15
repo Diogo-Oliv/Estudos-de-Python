cdf3bd4da922847f27e77d833eb18992200cb165
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 15:52:44 -0300

    Update Exercicio_5.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_5.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_5.py
@@ -4,15 +4,20 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
+# Escolha do caractere pelo usuário (podendo ser tanto um número quanto um símbolo).
 caractere = input("Insira o caractere desejado para construir o triângulo: ")
 
+# Laço para iterar as linhas.
 for linha in range(1, 6):
     print(caractere * linha)
 
+# Comando para inverter ou não o triângulo
 permissao = input("Gostaria de ver o triângulo de ponta cabeça? [S/N]").uper()
 
+# Condicional para realizar a inversão.
 if permissao == "S":
 
+    # Laço for realizando a iteração inversa.
     for linha in range(5, 0, -1):
         print(caractere * linha)
 
