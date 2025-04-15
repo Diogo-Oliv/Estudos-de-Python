a81954bb0bba75a2c2b6286136223aed26d28113
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 14:32:16 -0300

    Create Exercicio_2.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_2.py
@@ -0,0 +1,5 @@
+# Exercício 2 - Laço While.
+
+# Escreva um programa que utiliza um laço While para contar até um certo número escolhido previamente.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
