5419f79d2bdb3fd67dcad47116d06b26b86f7cd8
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-15 15:29:50 -0300

    Create Exercicio_5.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_5.py
@@ -0,0 +1,6 @@
+# Exercício 5 - Desenho com Laços.
+
+# Escreva um programa que utilize de laços for para a imprimir um triângulo retângulo com o caractere desejado.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
