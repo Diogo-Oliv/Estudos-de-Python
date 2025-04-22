84319dde9a101f66f267dcc589d3a5e0394528af
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-22 14:33:25 -0300

    Update Exercicio_6.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_6.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_6.py
@@ -4,3 +4,26 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
+# Variáveis iniciais.
+num1 = 0
+num2 = 1
+
+# limite estipulado pelo usuário.
+limite = int(input("Digite um número para o limite da sequência de Fibonacci: "))
+
+# Valor inicial da contagem de Fibonacci = 0.
+print(num1)
+
+# Laço for para a iteração da sequência.
+for i in range (2, limite + 1):
+    # Soma dos dois primeiros números.
+    somador = num1 + num2
+
+    # Vai printar na tela o valor do somador sempre que houver um número novo.
+    print(somador, " ")
+
+    # O valor inicialmente de 0 se torna 1
+    num1 = num2
+
+    # O valor de 1 se torna a soma de seus valores anteriores.
+    num2 = somador
