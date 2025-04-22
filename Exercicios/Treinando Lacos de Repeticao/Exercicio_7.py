a81aa4ec20ba4d8405b84bdfce42e065a7191df6
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-22 14:35:18 -0300

    Create Exercicio_7.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_7.py
@@ -0,0 +1,5 @@
+# Exercício 7 - Números Primos.
+
+# Escreva um programa que imprima apenas os números primos até um limite estipulado pelo usuário
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
