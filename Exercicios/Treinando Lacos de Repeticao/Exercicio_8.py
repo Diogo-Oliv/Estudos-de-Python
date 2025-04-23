b8d3a53f3d16719d1c55e50b3bb558301576fe49
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-23 15:25:48 -0300

    Update Exercicio_8.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_8.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_8.py
@@ -6,3 +6,22 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
+# Palavra ou frase para o usuário inserir
+texto = input("Digite uma palavra ou uma frase para a verificação de um palíndromo: ").lower()
+
+# Lista de caracteres inicialmente vazia para uma futura comparação.
+textoLista = []
+
+# Laço for para iterar todos os caracteres da palavra ou frase.
+for i in texto:
+    # Adiciona cada caractere na lista.
+    if i == ' ':
+        continue
+    else:
+        textoLista.append(i)
+
+# Comparação de listas normal e inversamente para ver se é um Palíndromo.
+if textoLista == textoLista[::-1]:
+    print('Essa palavra ou frase é um Palíndromo.')
+else:
+    print('Essa palavra ou frase não é um Palíndromo')
