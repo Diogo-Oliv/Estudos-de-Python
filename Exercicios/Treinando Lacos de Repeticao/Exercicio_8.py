ce7dc67bc849360d4100856d2a083fc1702a0a84
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-23 15:08:51 -0300

    Create Exercicio_8.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_8.py
@@ -0,0 +1,8 @@
+# Exercício 8 - Palíndromo.
+
+# Escreva um programa que peça ao usuário uma palavra ou frase e imprima se a entrada é um palíndromo ou não.
+
+# Palíndromo = palavra, frase ou sequência que permanece a mesma quando lida de trás para frente
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
