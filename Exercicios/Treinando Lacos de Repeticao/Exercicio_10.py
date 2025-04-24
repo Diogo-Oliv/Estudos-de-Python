8ea62ddf01f7536d1f4ec03b98cf758504f074cb
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-24 15:16:46 -0300

    Update Exercicio_10.py

--- Exercicios/Treinando Lacos de Repeticao/Exercicio_10.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_10.py
@@ -4,5 +4,22 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
+# Lista de valores desordenadas.
+numeros = [1, 5, 9, 3, 4, 2, 5, 1, 10, 4, 3]
 
+# Variável que receberá o tamanho da lista.
+num = len(numeros)
 
+# Laço que itera todos os elementos da lista.
+for i in range(num - 1):
+
+    # Laço que compara os elementos adjacentes e realiza as trocas de posição.
+    for j in range(num - 1 - i):
+
+        # Compara dois elementos adjacentes na lista.
+        if numeros[j] > numeros[j + 1]:
+
+            # Realiza a troca dos valores conforme a condicional anterior.
+            numeros[j], numeros[j + 1] = numeros[j + 1], numeros[j]
+
+print(numeros)
