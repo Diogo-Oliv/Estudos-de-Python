52f1d7ae5979a8435758a1646255f64c87b6286b
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-24 05:33:18 -0300

    Create Exercício_9.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercício_9.py
@@ -0,0 +1,6 @@
+# Exercício 9 - Decimal para Binário.
+
+# Escreva um programa que peça um número ao usuário e realize a conversão para binário.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
