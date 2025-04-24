a29c5656caa4af6a0e39f3bab49162be986189c3
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-24 05:33:33 -0300

    Update Exercício_9.py

--- Exercicios/Treinando Lacos de Repeticao/Exercício_9.py
+++ Exercicios/Treinando Lacos de Repeticao/Exercício_9.py
@@ -4,3 +4,23 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
 
+# Lista que receberá os valores da divisão em binário.
+listaBinario = []
+
+# Variável para inserir o número inteiro.
+numero = int(input("Digite um número inteiro: "))
+
+# Laço de repetição até o número chegar a 0.
+while numero > 0:
+    
+    # Variável receberá o resto da divisão por 2.
+    binario = numero % 2
+
+    # Adiciona o valor à uma lista
+    listaBinario.append(binario)
+
+    # Número sofrerá a DIVISÃO INTEIRA (descarta a parte decimal).
+    numero = numero // 2
+
+# Imprimi a lista ao inverso para mostrar o número em Binário corretamente.
+print(listaBinario[::-1])
