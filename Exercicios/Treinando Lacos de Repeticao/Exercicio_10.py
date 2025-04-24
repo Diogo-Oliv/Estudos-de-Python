0d49c0abae08b3d3a850f9de90f518ac14614cc3
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-24 15:09:43 -0300

    Create Exercicio_10.py

---
+++ Exercicios/Treinando Lacos de Repeticao/Exercicio_10.py
@@ -0,0 +1,8 @@
+# Exercício 10 - Ordenação de valores.
+
+# Escreva um programa que receba uma lista de números e ordene os valores em ordem crescente.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
+
+
