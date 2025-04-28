3986b2ed2f1b035ae444f4c59174c2e683d18d52
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 17:16:46 -0300

    Update Exercicio_6.py

--- Exercicios/List Comprehension/Exercicio_6.py
+++ Exercicios/List Comprehension/Exercicio_6.py
@@ -1,5 +1,9 @@
 # Exercício 6 - List Comprehension.
 
-# 
+# Crie uma list comprehension aninhada para gerar uma matriz 5x5 (uma lista de 5 listas, cada uma com 5 elementos) 
+# onde o valor de cada elemento seja a soma dos seus índices (linha + coluna).
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+matriz = [[linha + coluna for coluna in range(5)] for linha in range(5)]
+
+print(matriz)
