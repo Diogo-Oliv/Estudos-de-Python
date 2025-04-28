e77be4abd5d03cfe44115b842acd2f75b7364215
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 13:19:25 -0300

    Update Exercicio_5.py

--- Exercicios/List Comprehension/Exercicio_5.py
+++ Exercicios/List Comprehension/Exercicio_5.py
@@ -3,3 +3,11 @@
 # Escreva um programa que receba uma lista de números e utilize list comprehension para retornar unicamente os números pares.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Lista de números.
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Itera a lista com a condicional de números pares.
+numerosPares = [numero for numero in numeros if numero % 2 == 0]
+
+print(numerosPares)
