30cd4df459369156d52f4246e683c78095cca6fa
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 11:50:02 -0300

    Update Exercicio_3.py

--- Exercicios/List Comprehension/Exercicio_3.py
+++ Exercicios/List Comprehension/Exercicio_3.py
@@ -4,3 +4,11 @@
 # para Fahrenheit. A fórmula para conversão é: (F = \frac{9}{5}C + 32).
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Temperaturas para realizar as converções.
+temperaturas_celsius = [0, 10, 20, 30, 40]
+
+# List Comprehension realizando a converção e iteração da lista das temperaturas em celcius.
+temperaturas_Fahrenheit = [(celcius * (9 / 5) + 32) for celcius in temperaturas_celsius]
+
+print(temperaturas_Fahrenheit)
