9d41b5b88048dbc0f3ba1e0408860cf6e0c747b5
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 13:13:45 -0300

    Create Exercicio_4.py

---
+++ Exercicios/List Comprehension/Exercicio_4.py
@@ -0,0 +1,5 @@
+# Exercício 4 - List Comprehension.
+
+# Escreva um programa que receba uma lista de nomes e use List Comprehension para retornar o comprimento de cada item da lista em uma nova lista.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
