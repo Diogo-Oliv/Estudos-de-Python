9b4b98b86adf53432a5fa561ebcc754aea437def
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 10:18:31 -0300

    Create Exercicio_2.py

---
+++ Exercicios/List Comprehension/Exercicio_2.py
@@ -0,0 +1,5 @@
+# Exercício 2 - List Comprehension.
+
+# Escreva um programa que receba uma lista de nomes e crie uma list comprehension gerando uma nova lista com todos os nomes em maiúsculo.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
