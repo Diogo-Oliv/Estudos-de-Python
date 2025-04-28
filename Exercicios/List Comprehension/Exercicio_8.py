95cf2bc6e3eb76cec884d55451016198f8e21658
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 17:23:01 -0300

    Create Exercicio_8.py

---
+++ Exercicios/List Comprehension/Exercicio_8.py
@@ -0,0 +1,6 @@
+# Exercício 8 - List Comprehension.
+
+# Dadas duas listas: nomes = ["Ricardo", "Mariana", "Júlia"] e sobrenomes = ["Silva", "Oliveira", "Souza"], crie uma list comprehension que
+# gere uma lista com o nome completo de cada pessoa (nome + " " + sobrenome).
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
