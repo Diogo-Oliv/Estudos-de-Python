83f40987a4a3c5a8e87045dcfdb3aaa34376855e
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 17:23:05 -0300

    Create Exercicio_10.py

---
+++ Exercicios/List Comprehension/Exercicio_10.py
@@ -0,0 +1,13 @@
+# Exercício 10 - List Comprehension.
+
+# Dada uma lista de frases: frases = ["sol e lua", "mar azul", "céu estrelado", "terra plana", "vento forte"]
+# Crie uma list comprehension aninhada que faça o seguinte: 
+# 1. Para cada frase na lista, divida-a em palavras.
+# 2. Filtre as palavras que têm mais de 3 letras.
+# 3. Para as palavras restantes, pegue apenas a primeira letra.
+# 4. O resultado final deve ser uma lista de listas (uma "matriz" de iniciais), onde cada sublista corresponde a uma frase original.
+
+#Dica: Você precisará de uma list comprehension externa para iterar pelas frases e uma list comprehension interna 
+# para processar as palavras de cada frase, aplicando o filtro e a transformação.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
