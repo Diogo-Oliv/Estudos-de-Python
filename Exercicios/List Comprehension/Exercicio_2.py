3c396243ac03efa3b9f8e7b15fa609bcce989e0c
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 10:18:38 -0300

    Update Exercicio_2.py

--- Exercicios/List Comprehension/Exercicio_2.py
+++ Exercicios/List Comprehension/Exercicio_2.py
@@ -3,3 +3,11 @@
 # Escreva um programa que receba uma lista de nomes e crie uma list comprehension gerando uma nova lista com todos os nomes em maiúsculo.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Lista de nomes.
+nomes = ["ana", "carlos", "maria", "joão"]
+
+# List Comprehension transformando cada nome para letras maiúsculas.
+nomesMaiusculos = [nome.upper() for nome in nomes]
+
+print(nomesMaiusculos)
