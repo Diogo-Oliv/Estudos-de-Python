c82c825517e1d5c5a624ead8a65a1e7efcd5b27e
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 17:22:59 -0300

    Create Exercicio_7.py

---
+++ Exercicios/List Comprehension/Exercicio_7.py
@@ -0,0 +1,7 @@
+# Exercício 7 - List Comprehension.
+
+# Dada a lista de números numeros = [-2, -1, 0, 1, 2], crie uma list comprehension que gere uma nova lista onde 
+# os números negativos sejam substituídos por 0, e os números não negativos permaneçam os mesmos.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
