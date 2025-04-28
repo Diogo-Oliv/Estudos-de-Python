15ee979071f778b0f2f0dedd1d66c620a0a37d4e
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 17:23:03 -0300

    Create Exercicio_9.py

---
+++ Exercicios/List Comprehension/Exercicio_9.py
@@ -0,0 +1,7 @@
+# Exercício 9 - List Comprehension.
+
+# Dada a lista de listas dados = [[1, 2], [3, 4, 5], [6]], crie uma list comprehension que "achate" 
+# essa lista em uma única lista contendo todos os números.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
+
