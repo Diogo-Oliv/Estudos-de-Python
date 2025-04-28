561973a854d877481dea15a26424c6c64e1ed2fb
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 13:15:35 -0300

    Update Exercicio_4.py

--- Exercicios/List Comprehension/Exercicio_4.py
+++ Exercicios/List Comprehension/Exercicio_4.py
@@ -3,3 +3,11 @@
 # Escreva um programa que receba uma lista de nomes e use List Comprehension para retornar o comprimento de cada item da lista em uma nova lista.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+# Lista de palavras.
+palavras = ["python", "é", "uma", "linguagem", "poderosa"]
+
+# Pega o tamanho de cada item iterado na lista de pelavras.
+comprimentoPalavras = [len(palavra) for palavra in palavras]
+
+print(comprimentoPalavras)
