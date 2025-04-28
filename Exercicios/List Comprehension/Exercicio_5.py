f4d662d40420f04320b79150789e90e16b14fd49
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 13:16:53 -0300

    Create Exercicio_5.py

---
+++ Exercicios/List Comprehension/Exercicio_5.py
@@ -0,0 +1,5 @@
+# Exercício 5 - List Comprehension.
+
+# Escreva um programa que receba uma lista de números e utilize list comprehension para retornar unicamente os números pares.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
