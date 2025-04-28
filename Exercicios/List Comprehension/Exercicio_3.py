d68e10662fb9bfced09fb4b867b3239520d2a2a2
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-28 11:46:39 -0300

    Create Exercicio_3.py

---
+++ Exercicios/List Comprehension/Exercicio_3.py
@@ -0,0 +1,6 @@
+# Exercício 3 - List Comprehension.
+
+# Escreva um programa que receba uma lista de temperaturas em Celsius, crie uma list comprehension que converta cada temperatura 
+# para Fahrenheit. A fórmula para conversão é: (F = \frac{9}{5}C + 32).
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
