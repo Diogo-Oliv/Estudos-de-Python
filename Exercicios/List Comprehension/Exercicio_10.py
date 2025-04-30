613feb4cffec0f72cac9f614898154a73014d6eb
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-04-30 03:32:42 -0300

    Update Exercicio_10.py

--- Exercicios/List Comprehension/Exercicio_10.py
+++ Exercicios/List Comprehension/Exercicio_10.py
@@ -5,7 +5,7 @@
 # 1. Para cada frase na lista, divida-a em palavras.
 # 2. Filtre as palavras que têm mais de 3 letras.
 # 3. Para as palavras restantes, pegue apenas a primeira letra.
-# 4. O resultado final deve ser uma lista de listas (uma "matriz" de iniciais), onde cada sublista corresponde a uma frase original.
+# Final: O resultado final deve ser uma lista de listas (uma "matriz" de iniciais), onde cada sublista corresponde a uma frase original.
 
 #Dica: Você precisará de uma list comprehension externa para iterar pelas frases e uma list comprehension interna 
 # para processar as palavras de cada frase, aplicando o filtro e a transformação.
@@ -15,6 +15,23 @@
 # Lista de frases.
 frases = ["sol e lua", "mar azul", "céu estrelado", "terra plana", "vento forte"]
 
-# O que fazer primeiro? 
-# Começar pelo 1. dividir as frases em palavras.
-frasesEmPalavras = [[]frases[palavra].split() for palavra in range(len(frases))]
+# Passo 1. Dividir as frases em palavras.
+listaDePalavras = [[palavra for palavra in frase.split()] for frase in frases]
+
+print("Passo 1. Dividir em palavras: ")
+print(listaDePalavras)
+print("Fim do primeiro passo.")
+
+# Passo 2. Filtrar as palavras com mais de 3 letras.
+listaDePalavras = [[palavra for palavra in frase.split() if len(palavra) > 3] for frase in frases]
+
+print("Passo 2. Filtrar palavras com mais de 3 letras: ")
+print(listaDePalavras)
+print("Fim do segundo passo.")
+
+# Passo 3. Pegando a Primeira Letra das Palavras Filtradas.
+listaDePalavras = [[palavra[0] for palavra in frase.split() if len(palavra) > 3] for frase in frases]
+
+print("Passo 3. mostrar apenas a primeira letra dessas palavras: ")
+print(listaDePalavras)
+print("Fim do terceiro passo.")
