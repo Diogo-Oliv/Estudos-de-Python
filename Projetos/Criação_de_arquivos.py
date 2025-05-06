81ce6a33cc3b5f0d9dd50ee03ce92d1ba55aca32
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-06 15:54:08 -0300

    Update Criação_de_arquivos.py

--- Projetos/Criação_de_arquivos.py
+++ Projetos/Criação_de_arquivos.py
@@ -1,34 +1,46 @@
+#-----------------------------------------------------------------------------------------------------------------------------------#
 # Criando arquivos para estudos de exercícios. PROTÓTIPO
-
+#-----------------------------------------------------------------------------------------------------------------------------------#
 # Apresentação do programa.
 print("----------------------------------------------------------------------")
 print("-----------BEM VINDO AO CRIADOR DE ARQUIVOS PERSONALIZADOS------------")
 print("----------------------------------------------------------------------")
-
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# Tratamento de erro durante a requisição do tipo de arquivo.
 tipoArquivo = False
 
 while tipoArquivo == False:
 
-    tipoArquivo = input("""Qual o tipo de arquivo que você gostaria de criar? 
-                        Exemplos:   || .txt || .py || .md || .html || .css ||""").lower()
+    # Variável que receberá o tipo do arquivo.
+    extencaoArquivo = input("""Qual o tipo de arquivo que você gostaria de criar? 
+Exemplos:   || .txt || .py || .md || .html || .css || \n \n""").lower()
     
-    if "." not in tipoArquivo:
-        print("O tipo do arquivo não apresentou o . antes do tipo.")
-        tipoArquivo = input("""Qual o tipo de arquivo que você gostaria de criar? 
-        Exemplos:   || .txt || .py || .md || .html || .css || """).lower()
+    # Caso o usuário esqueça de inserir o . antes do início do tipo do arquivo.
+    if "." not in extencaoArquivo:
+       
+       # Realizará a notificação do erro e apresentará novamente.
+       print("O tipo do arquivo não apresentou o . antes do tipo.")
+       extencaoArquivo = input("""Qual o tipo de arquivo que você gostaria de criar? 
+Exemplos:   || .txt || .py || .md || .html || .css || \n \n""").lower()
+    
+    # Caso esteja de acordo com o padrão, sairá do laço While e seguirá com o resto das requisições.
     else:
         tipoArquivo = True
+#-----------------------------------------------------------------------------------------------------------------------------------#
+# Função para o método de Criação de arquivos.
+def CriandoArquivos(extencaoArquivo):
 
-def CriandoArquivos(tipoArquivo):
-
+    # Tratamento de erro durante a requisição do path escolhido.
+    caminhoArquivo = input("Insira o path onde se localizará o novo arquivo criado: \n \n")
+    
     # Laço for para controlar a quantidade de arquivos criados.
     for i in range(3,5):
 
         # Usar o open com o mode x para criar o arquivo e escrever o número do exercício tanto no diretório quanto dentro dele utilizando o write.
-        with open(file="E:\Diogo\MeusProjetos\Estudos-de-Python\Exercicios\Dict Comprehension\Exercicio_" + str(i) + tipoArquivo, mode="x") as arquivo:
+        with open(file=f"{caminhoArquivo}\Exercicio_{str(i)}{extencaoArquivo}", mode="x") as arquivo:
         
-            arquivo.write("# Exercício " + str(i) + " - Dictionary Comprehension. \n")
-            arquivo.write("\n")
+            arquivo.write("# Exercicio " + str(i) + " - Dictionary Comprehension. \n \n")
+            arquivo.write("\n \n")
             arquivo.write("#-----------------------------------------------------------------------------------------------------------------------------------#")
 
-CriandoArquivos(tipoArquivo)
+CriandoArquivos(extencaoArquivo)
