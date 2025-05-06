fd2f20afb62938d4eda84416db1eb0289b51ec2b
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-06 19:09:05 -0300

    Update Testando.py

--- Projetos/Testando.py
+++ Projetos/Testando.py
@@ -1,18 +1,23 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("GUI Personalizada")
+        self.setWindowTitle("Criador de Arquivos")
         self.setGeometry(2500, 350, 500, 500)
 
-        label = QLabel("Hello", self)
-        label.setFont(QFont("Arial", 30))
-        label.setGeometry(0,0, 500, 100)
-        label.setStyleSheet("color: blue;")
+        # Criação da label que mostrará o Cabeçalho do programa.
+        labelHeader = QLabel("Programa para criação de arquivos", self)
+        labelHeader.setFont(QFont("Arial", 15))
+        labelHeader.setGeometry(0,0, 500, 100)
+        labelHeader.setStyleSheet("background-color: gray;"
+                                  "font-weight: bold;"
+                                  "text-decorantion: underline;")
+        labelHeader.setAlignment(Qt.AlignVCenter)
 
 def main():
     app = QApplication(sys.argv)
