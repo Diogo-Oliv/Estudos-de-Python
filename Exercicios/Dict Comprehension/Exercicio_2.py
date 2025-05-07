06b65379f1a8c583bc1d4f51585e0639367aa3fd
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-07 00:48:48 -0300

    Delete Exercicio_2.py

--- Exercicios/Dict Comprehension/Exercicio_2.py
+++
@@ -1,5 +0,0 @@
-# Exercício 2 - Dictionary Comprehension.
-
-# Crie um dictionary comprehension que gere um dicionário com os números de 1 a 5 (inclusive) como chaves e seus cubos como valores.
-
-#-----------------------------------------------------------------------------------------------------------------------------------#
