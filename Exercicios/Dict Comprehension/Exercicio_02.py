22fea094e2aa676a966af7c4e114a1c94ff5e889
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-08 19:34:15 -0300

    Exercicios criados

---
+++ Exercicios/Dict Comprehension/Exercicio_02.py
@@ -0,0 +1,5 @@
+# Exercício 2 - Dict Comprehension.
+
+# Dada a lista de nomes nomes = ["ana", "carlos", "maria", "joão"], crie um dictionary comprehension que gere um dicionário onde as chaves são os nomes e os valores são o comprimento de cada nome.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
---
+++ Exercicios/Dict Comprehension/Exercicio_05.py
@@ -0,0 +1,5 @@
+# Exercicio 5 - Dict Comprehension.
+
+# Dada a lista de tuplas alunos = [("Ana", 8.5), ("Carlos", 7.0), ("Mariana", 9.2), ("João", 6.5)], crie um dictionary comprehension que gere um dicionário onde as chaves são os nomes dos alunos e os valores são "Aprovado" se a nota for maior ou igual a 7.0, e "Reprovado" caso contrário.
+
+#-----------------------------------------------------------------------------------------------------------------------------------#
