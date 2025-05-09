f8f2bccb407bfa47f2e52a811b66209ef0706bdd
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-09 19:14:19 -0300

    Update Exercicio_01.py

--- Exercicios/Dict Comprehension/Exercicio_01.py
+++ Exercicios/Dict Comprehension/Exercicio_01.py
@@ -3,3 +3,7 @@
 # Crie um dictionary comprehension que gere um dicionário com os números de 1 a 5 (inclusive) como chaves e seus cubos como valores.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+cubos = {numero: numero**3 for numero in range(1, 6)}
+
+print(cubos)
