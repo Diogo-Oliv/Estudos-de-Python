00f61512e512497f481dbd76f9f6a11a62576104
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-09 19:14:24 -0300

    Update Exercicio_04.py

--- Exercicios/Dict Comprehension/Exercicio_04.py
+++ Exercicios/Dict Comprehension/Exercicio_04.py
@@ -3,3 +3,9 @@
 # Dada a string texto = "abracadabra", crie um dictionary comprehension que conte a frequência de cada letra na string e gere um dicionário onde as chaves são as letras e os valores são suas respectivas contagens.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+texto = "abracadabra"
+
+freqLetras = {letra: texto.count(letra) for letra in texto}
+
+print(freqLetras)
