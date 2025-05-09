cfd0baa0d96796d1638c68490422cec5876733c7
Diogo Oliveira <diogo.oliveira.mp@gmail.com>
2025-05-09 19:14:22 -0300

    Update Exercicio_02.py

--- Exercicios/Dict Comprehension/Exercicio_02.py
+++ Exercicios/Dict Comprehension/Exercicio_02.py
@@ -3,3 +3,9 @@
 # Dada a lista de nomes nomes = ["ana", "carlos", "maria", "joão"], crie um dictionary comprehension que gere um dicionário onde as chaves são os nomes e os valores são o comprimento de cada nome.
 
 #-----------------------------------------------------------------------------------------------------------------------------------#
+
+nomes = ["ana", "carlos", "maria", "joão"]
+
+dictNome = {nome: len(nome) for nome in nomes}
+
+print(dictNome)
